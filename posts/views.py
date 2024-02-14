d0a7e289dad47e6673025873d15c788ded278ba7
[joel] <[jbur645@wgu.edu]>
2024-02-14 11:40:09 -0500

    permissions modified, start with strict project level permissions, then modify view level permissions as needed

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
+from .permissions import IsAuthOrReadOnly
 from .serializers import PostSerializer
 
 
 class PostList(generics.ListCreateAPIView):
+    # permission_classes = (permissions.IsAuthenticated,) view level permission set
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = (permissions.IsAuthenticated,) view level permission set
+    permission_classes = (IsAuthOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
